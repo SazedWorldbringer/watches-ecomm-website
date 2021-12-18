3a734a8ee44ecf928631ff843bf060a43583161f
Atharva Pardeshi <atharva.exe@gmail.com>
2021-12-18 09:32:11 +0530

    Added Header, Nav, Home, and Featured page. Added some basic JavaScript for the Header and Nav

---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+/*========== SHOW MENU ==========*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+      
+/*========== MENU VISIBLE ==========*/
+/* Validate if constant exists */
+if(navToggle) {
+    navToggle.addEventListener('click', function() {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*========== MENU HIDDEN ==========*/
+/* Validate if constant exists */
+if(navClose) {
+    navClose.addEventListener('click', function() {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*========== REMOVE MENU MOBILE ==========*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(i => i.addEventListener('click', linkAction))
+
+/*========== CHANGE BACKGROUND HEADER ==========*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
