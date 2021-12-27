e3c8be54164728c947aa50f455b0245d57e48632
Atharva Pardeshi <atharva.exe@gmail.com>
2021-12-27 16:12:13 +0530

    Added the Testimonials, New, Newsletter and Cart sections, styled them, added dark/light theme and other JavaScript functionalities(basic). Finished with the website! :)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
 /*========== SHOW MENU ==========*/
+
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
       
 /*========== MENU VISIBLE ==========*/
+
 /* Validate if constant exists */
 if(navToggle) {
     navToggle.addEventListener('click', function() {
@@ -12,6 +14,7 @@ if(navToggle) {
 }
 
 /*========== MENU HIDDEN ==========*/
+
 /* Validate if constant exists */
 if(navClose) {
     navClose.addEventListener('click', function() {
@@ -20,6 +23,7 @@ if(navClose) {
 }
 
 /*========== REMOVE MENU MOBILE ==========*/
+
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
@@ -30,6 +34,7 @@ function linkAction(){
 navLink.forEach(i => i.addEventListener('click', linkAction))
 
 /*========== CHANGE BACKGROUND HEADER ==========*/
+
 function scrollHeader() {
     const header = document.getElementById('header')
     // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
@@ -37,13 +42,118 @@ function scrollHeader() {
 }
 window.addEventListener('scroll', scrollHeader)
 
+/*========== TESTIMONIAL SWIPER ==========*/
 
+let testimonialSwiper = new Swiper(".testimonial-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
 
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
+/*========== NEW SWIPER ==========*/
 
+let newSwiper = new Swiper(".new-swiper", {
+    spaceBetween: 20,
+    loop: 'true',
 
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
+});
 
+/*========== SCROLL SECTIONS ACTIVE LINK ==========*/
 
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop -50,
+              sectionId = current.getAttribute('id')
+
+              if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                  document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.add('active-link')
+              } else {
+                  document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.remove('active-link')
+              }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*========== SHOW SCROLL UP ==========*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll
+    if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*========== SHOW CART ==========*/
+const cart = document.getElementById('cart'),
+      cartShop = document.getElementById('cart-shop'),
+      cartClose = document.getElementById('cart-close')
+      
+/*========== CART VISIBLE ==========*/
+
+/* Validate if constant exists */
+if(cartShop) {
+    cartShop.addEventListener('click', function() {
+        cart.classList.add('show-cart')
+    })
+}
+
+/*========== CART HIDDEN ==========*/
+
+/* Validate if constant exists */
+if(cartClose) {
+    cartClose.addEventListener('click', function() {
+        cart.classList.remove('show-cart')
+    })
+}
+
+/*========== DARK LIGHT THEME ==========*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (is user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled,we as what the issue was to know if we activated or deactivated the dark theme
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate/deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 
 
